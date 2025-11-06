289ad97cc4bada64d2a4f6e236d565abf46c5270
kramy <kramy@LAPTOP-H8QAK69L>
2025-11-06 14:32:51 +0530

    commit

---
+++ spring-autowiring/src/main/java/com/tnsif/spring_autowiring/SpringAutowiringApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_autowiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutowiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutowiringApplication.class, args);
+	}
+
+}
