b14cd17503768b3d3e364ee78a2b87282c08d4f6
kramy <kramy@LAPTOP-H8QAK69L>
2025-11-09 19:44:23 +0530

    commit

---
+++ spring-ioc-demo/src/main/java/com/tnsif/spring_ioc_demo/Apps.java
@@ -0,0 +1,9 @@
+package com.tnsif.spring_ioc_demo;
+
+public class Apps {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ spring-ioc-demo/src/test/java/com/tnsif/spring_ioc_demo/SpringIocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_ioc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
