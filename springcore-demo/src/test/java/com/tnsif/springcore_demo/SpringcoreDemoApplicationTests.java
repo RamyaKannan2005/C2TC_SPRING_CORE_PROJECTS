67fb807d6620a0ed69b4d11955ccd59172224497
kramy <kramy@LAPTOP-H8QAK69L>
2025-11-09 19:55:52 +0530

    commit

---
+++ springcore-demo/src/test/java/com/tnsif/springcore_demo/SpringcoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.springcore_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
